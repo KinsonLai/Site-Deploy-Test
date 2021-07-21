8018cdeebe20ccd2ef1e5e948f7327e05a26c441
KinsonLai <67530699+KinsonLai@users.noreply.github.com>
2021-07-21 21:41:02 +0800

    add Freezer

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from website import create_app
+from flask_frozen import Freezer
+import sys
 
 app = create_app()
+freezer = Freezer(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        print("Building website...")
+        freezer.freeze()
+    else:
+        app.run(debug=True)
