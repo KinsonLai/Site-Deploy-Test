f76edd8b06db817a53d675f71a9691f98dc3b861
KinsonLai <67530699+KinsonLai@users.noreply.github.com>
2021-07-23 11:53:55 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -17,7 +17,10 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                if User.first_name == "Rick":
+                    return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+                else:
+                    return redirect(url_for("views.home"))
             else:
                 flash('Incorrect password, try again!', category='error')
         else:
